5d82d840e0c92d81313240b2822710e8ed22d30c
Makibu <bumaciej@gmail.com>
2024-08-04 11:47:39 +0200

    bg attachment fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import PageContent from "./components/PageContent.jsx";
 function App(){
     return (
         <div
-            className="font-anonPro absolute min-h-[100svh] w-full bg-[#0C0C0C] bg-[radial-gradient(#e5e7eb_0.2px,transparent_1px)] [background-size:32px_32px] flex flex-col items-center">
+            className="font-anonPro absolute min-h-[100svh] w-full bg-[#0C0C0C] bg-[radial-gradient(#e5e7eb_0.2px,transparent_1px)] [background-size:32px_32px] flex flex-col items-center bg-fixed">
             <PageContent/>
         </div>
     )
